5c18d08ab5f396aca5dd7cfd5878ffbcd9a55716
Nicola Marella <marella.nicola@gmail.com>
2018-11-17 16:51:31 +0100

    updated settings to server statics file via unicorn (only for dev). Updated reqs

--- home_smart_home/settings/base.py
+++ home_smart_home/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = ')b4busv$l$$@yxver1!e8!5s*wum^$2*)f3nz+&+blgt#7^4k$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
